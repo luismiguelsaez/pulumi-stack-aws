902c68ea606d37c0c9aad49d4416ca3495279824
Luis Miguel Sáez Martín <luismiguelsaez83@gmail.com>
2023-10-15 14:14:22 +0200

    Added exports

--- helm/system/main/__main__.py
+++ helm/system/main/__main__.py
@@ -1,1 +1,14 @@
 from resources import helm, iam, k8s
+from pulumi import export, Output
+import yaml
+
+"""
+Export new `aws-auth` ConfigMap contents
+"""
+export("karpenter-aws-auth-cm", k8s.new_roles_obj.apply(lambda roles: yaml.dump(roles, default_flow_style=False)))
+
+"""
+Export Helm releases
+"""
+export("helm_release_karpenter", helm.karpenter_helm_release.status)
+export("helm_release_cluster_autoscaler",helm.cluster_autoscaler_helm_release.status)

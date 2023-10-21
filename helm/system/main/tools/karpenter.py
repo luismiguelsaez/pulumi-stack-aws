dfac4893f6221d034b0751063082446357a6d979
Luis Miguel Sáez Martín <luismiguelsaez83@gmail.com>
2023-10-21 10:20:06 +0200

    Added Jinja templates for Karpenter

--- helm/system/main/tools/karpenter.py
+++ helm/system/main/tools/karpenter.py
@@ -3,6 +3,7 @@ from pulumi_kubernetes import Provider
 from pulumi import ResourceOptions, Output
 from os import path
 import glob
+import jinja2
 
 def karpenter_templates(
     name: str,
@@ -17,19 +18,25 @@ def karpenter_templates(
 
     def transform_manifest(obj, opts):
         sg_selector=sg_selector_tags
-        #{
-        #    "karpenter.sh/discovery": eks_cluster_name.apply(lambda name: name)
-        #}
-
         subnet_selector=subnet_selector_tags
-        #{
-        #    "karpenter.sh/discovery": eks_cluster_name.apply(lambda name: name)
-        #}
 
         obj['spec']['securityGroupSelector'] = sg_selector
         obj['spec']['subnetSelector'] = subnet_selector
-        
-        #obj['spec']['userData'] = ""
+
+    files_jinja = [
+        path.join(path.dirname("__file__"), file) for file in glob.glob(path.join(path.dirname("__file__"), manifests_path, "*.j2"))
+    ]
+    
+    for file in files_jinja:
+      with open(file, 'r') as f:
+        file_contents = f.read()
+        rendered_file = jinja2.Template(file_contents).render(
+          ssh_public_key=ssh_public_key,
+          sg_selector_tags=sg_selector_tags,
+          subnet_selector_tags=subnet_selector_tags,
+        )
+      with open(file.replace(".j2", ".yaml"), 'w') as f:
+        f.write(rendered_file)
 
     files = [
         path.join(path.dirname("__file__"), file) for file in glob.glob(path.join(path.dirname("__file__"), manifests_path, "*.yaml"))

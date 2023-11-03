94a4f07c35c2b31817a03ad48821a29510fc15c6
Luis Miguel Sáez Martín <luismiguelsaez83@gmail.com>
2023-11-03 12:46:51 +0100

    Added task definition

--- ecs/main/resources/ecs.py
+++ ecs/main/resources/ecs.py
@@ -1,6 +1,7 @@
 from resources import elb
 from stack import network, name_prefix, env
 from pulumi_aws import ecs
+import json
 
 ecs_cluster = ecs.Cluster(
     name=name_prefix,
@@ -35,11 +36,36 @@ ecs_cluster_capacity_providers = ecs.ClusterCapacityProviders(
     ],
 )
 
+ecs_cluster_service_test_task_definition_nginx = ecs.TaskDefinition(
+    resource_name=f"ecs-cluster-{name_prefix}-test-nginx",
+    cpu="256",
+    memory="512",
+    network_mode="awsvpc",
+    family=f"ecs-cluster-{name_prefix}-test-nginx",
+    container_definitions=json.dumps([
+        {
+            "name": "nginx",
+            "image": "nginx:1.25.3-alpine",
+            "cpu": 10,
+            "memory": 256,
+            "essential": True,
+            "portMappings": [{
+                "containerPort": 80,
+                "hostPort": 80,
+            }],
+        },
+    ]),
+    tags={
+        "Name": f"ecs-cluster-{name_prefix}-test",
+    },
+)
+
 ecs_cluster_service_test = ecs.Service(
     resource_name=f"ecs-cluster-{name_prefix}-test",
     name=f"ecs-cluster-{name_prefix}-test",
     cluster=ecs_cluster.name,
     desired_count=1,
+    task_definition=ecs_cluster_service_test_task_definition_nginx.arn,
     load_balancers=[
         ecs.ServiceLoadBalancerArgs(
             target_group_arn=elb.ecs_elb_target_group_test.arn,
